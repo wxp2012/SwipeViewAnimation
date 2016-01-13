a3c3f6e7c6d81c51588cbcd7d8cae5ede692b4e5
xp mac <xp_mac@xp-macdemini.lan>
2016-01-13 14:38:40 +0800

    Initial Commit

---
+++ SwipeViewAnimation/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SwipeViewAnimation
+//
+//  Created by xp_mac on 16/1/13.
+//  Copyright © 2016年 xp_mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
